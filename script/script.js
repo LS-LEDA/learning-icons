925c1a0f7a73932de812e87f9e4b286e70079e79
Aleix <aleix.olle@students.salle.url.edu>
2022-05-28 12:01:13 +0200

    back to top button added

--- script/script.js
+++ script/script.js
@@ -1,7 +1,26 @@
-function readJSON(){
-    var category = document.getElementById("category");
-    var name = document.getElementById("icon_name");
 
+//Get the button:
+var mybutton = document.getElementById("myBtn");
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    var mybutton = document.getElementById("myBtn");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
+
+
+function readJSON(){
     var request = new XMLHttpRequest();
     request.open("GET", "../../icons.json", false);
     request.send(null)
@@ -9,23 +28,19 @@ function readJSON(){
     
 
     for (let i = 0; i < icons.info.length; i++){
-        
         document.body.innerHTML += '<div id = "' + icons.info[i].category +  '">'
-        
-        document.getElementById(icons.info[i].category).innerHTML += '<h1>' + icons.info[i].category + '</h1>';
-        
-        for(let y = 0; y < icons.info[i].icon.length; y++){
-            document.getElementById(icons.info[i].category).innerHTML += '<h2>' + icons.info[i].icon[y].name + '</h2>';
-            document.getElementById(icons.info[i].category).innerHTML += '<p>' + icons.info[i].icon[y].description + '</p>';
-            document.getElementById(icons.info[i].category).innerHTML +=  '<img src="' + icons.info[i].icon[y].url + '" alt="Girl in a jacket" width="100" height="100">'; 
-        }
-        
-    }
+        var division = document.getElementById(icons.info[i].category);
 
-    
+
+        division.innerHTML += '<h1>' + icons.info[i].category + '</h1>';
+        for(let y = 0; y < icons.info[i].icon.length; y++){
+            division.innerHTML += '<div id = "' + icons.info[i].icon[y].name +  '" class = "subdivision">'
+            var subdivision = document.getElementById(icons.info[i].icon[y].name);
+            subdivision.innerHTML += '<h2>' + icons.info[i].icon[y].name + '</h2>';
+            subdivision.innerHTML += '<p>' + icons.info[i].icon[y].description + '</p>';
+            subdivision.innerHTML +=  '<img src="' + icons.info[i].icon[y].url + '" alt="Girl in a jacket" width="100" height="100">'; 
+        }
+    }
 }
 
-
-
-//name.innerHTML += icons.info[i].icon[y].name + "<br>";
 
