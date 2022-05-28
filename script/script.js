76b011df48f85eb884e9401291ad97ee137f07e7
Aleix <aleix.olle@students.salle.url.edu>
2022-05-28 12:50:07 +0200

    fonts added

--- script/script.js
+++ script/script.js
@@ -18,8 +18,7 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 } 
-
-
+6
 function readJSON(){
     var request = new XMLHttpRequest();
     request.open("GET", "../../icons.json", false);
@@ -36,9 +35,9 @@ function readJSON(){
         for(let y = 0; y < icons.info[i].icon.length; y++){
             division.innerHTML += '<div id = "' + icons.info[i].icon[y].name +  '" class = "subdivision">'
             var subdivision = document.getElementById(icons.info[i].icon[y].name);
-            subdivision.innerHTML += '<h2>' + icons.info[i].icon[y].name + '</h2>';
-            subdivision.innerHTML += '<p>' + icons.info[i].icon[y].description + '</p>';
-            subdivision.innerHTML +=  '<img src="' + icons.info[i].icon[y].url + '" alt="Girl in a jacket" width="100" height="100">'; 
+            subdivision.innerHTML += '<h2 class = "name">' + icons.info[i].icon[y].name + '</h2>';
+            subdivision.innerHTML += '<p class = "description">' + icons.info[i].icon[y].description + '</p>';
+            subdivision.innerHTML +=  '<img class = "image" src="' + icons.info[i].icon[y].url + '" alt="Girl in a jacket" width="100" height="100">'; 
         }
     }
 }
